ce08ea4bdb3be95c9932f6ec4fd527e3a5eab1dd
Mechta <orazbai.alibek@gmail.com>
2026-04-27 18:15:28 +0500

    MinHeap FINALLY COMPLETED PROPERLY

--- src/MinHeap.java
+++ src/MinHeap.java
@@ -9,6 +9,9 @@ public class MinHeap implements IntQueue{
     @Override
     public void add(int item) {
         arr.add(item);
+        for (int j = arr.size() - 1; j >= 0; j--){
+            Compare(j);
+        }
     }
 
     @Override
@@ -18,12 +21,36 @@ public class MinHeap implements IntQueue{
 
     @Override
     public int remove() {
-        return 0;
+        return arr.remove(arr.size() - 1);
+    }
+
+    public void Compare(int i){
+        int child = arr.get(i);
+        int parent = arr.get((i-1)/2);
+        if (parent < child) return;
+
+        if (parent > child) {
+            int j = parent;
+            arr.set((i-1)/2, child);
+            arr.set(i, j);
+            Compare(i - 1);
+        }
+    }
+
+    @Override
+    public String toString(){
+        return arr.toString();
     }
 
     ///  main method to test the class.
     static void main() {
         // test the MinHeap class by using it to sort an array in descending order
+        MinHeap min = new MinHeap();
+        min.add(12);
+        min.add(5);
+        min.add(6);
+        min.add(1);
 
+        System.out.println(min);
     }
 }
